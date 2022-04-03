2d9b3b63ac9e8c692996d624fc10e73c71d43c70
James Medved <jamesmedved217@gmail.com>
2022-04-03 17:41:07 -0400

    Adds paddle movement

--- client.py
+++ client.py
@@ -1,28 +1,26 @@
 import socket
 
-HEADER = 64
-PORT = 8888
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "marvin.webredirect.org"
-ADDR = (SERVER, PORT)
+class Client():
+    def __init__(self, header=64, port=8888, format='utf-8', disconnect_mess='!DISCONNECT',
+                 server='marvin.webredirect.org'):
+        self.header = header
+        self.port = port
+        self.format = format
+        self.disconnect_message = disconnect_mess
+        self.server = server
+        self.address = (server, port)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.address)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    def send_msg(self, msg):
+        message = msg.encode(self.format)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(self.format)
+        send_length += b' ' * (self.header - len(send_length))
+        self.client.send(send_length)
+        self.client.send(message)
+        print(self.client.recv(2048).decode(self.format))
 
-send("The N-Word!")
-input()
-send("Hello Everyone!")
-input()
-send("Hello Tim!")
-
-send(DISCONNECT_MESSAGE)
+    def disconnect(self):
+        self.send_msg(self.disconnect_message)
