aae149cbb2289a77ad222b064f3b2d13532850d8
EngimaticThrakon <gbrown92799@gmail.com>
2022-04-04 12:36:15 -0400

    Got threaded program working and used CMake for it

--- server-dev/port_listener/socket-listener-threading.cpp
+++ server-dev/port_listener/socket-listener-threading.cpp
@@ -21,20 +21,22 @@
 #define FALSE 0
 #define PORT 8888
 
-int client_socket[30] = {0};
+const int max_clients = 2;
+int client_socket[max_clients] = {0};
 std::string reply = "<0:0,0:0,0>";
 std::mutex redis_mtx;
 redis_handler *redisHandler;
 
 struct sockaddr_in address;
 fd_set readfds;
+int max_sd;
 
 void *status_update(int socket_id)
 {
     while (true)
     {
         redis_mtx.lock();
-        redisHandler->get_key(std::to_string(i) + ':' + _player_connected);
+        redisHandler->get_key(std::to_string(socket_id) + ':' + _player_connected);
         //redisHandler->get_key(std::to_string(i) + ':' + _player_connected);
         redis_mtx.unlock();
 
@@ -45,18 +47,20 @@ void *status_update(int socket_id)
     return NULL;
 }
 
-void *client_listener(int max_sd, fd_set readfds, int socket_id)
+void *client_listener(void *arg)
 {
+    int socket_id = *reinterpret_cast<int *>(arg);
     redis_mtx.lock();
-    redisHandler->set_key(std::to_string(i) + ':' + _player_connected, std::to_string(1));
+    redisHandler->set_key(std::to_string(socket_id) + ':' + _player_connected, std::to_string(1));
     redis_mtx.unlock();
 
     bool connected = true;
     char buffer[1024];
+    int activity = 0;
 
     while (connected)
     {
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        activity = select(3, &readfds, NULL, NULL, NULL);
 
         if ((activity < 0) && (errno != EINTR))
         {
@@ -68,20 +72,20 @@ void *client_listener(int max_sd, fd_set readfds, int socket_id)
             int temp = read(socket_id, buffer, 1025);
             buffer[temp] = '\0';
 
-            if (std::string(buffer).substr(0, 3) = "~~~")
+            if (std::string(buffer).substr(0, 3) == "~~~")
             {
                 connected = false;
             }
             else
             {
-                y_pos = atoi(buffer);
-                reply = '<' + std::to_string(y_pos) + ':' + std::to_string(ball_x) + ',' + std::to_string(ball_y) + ':' + std::to_string(score_a) + ',' + std::to_string(score_b) + '>';
+                int y_pos = atoi(buffer);
+                //reply = '<' + std::to_string(y_pos) + ':' + std::to_string(ball_x) + ',' + std::to_string(ball_y) + ':' + std::to_string(score_a) + ',' + std::to_string(score_b) + '>';
                 // std::string(y_pos + ':' + ball_x + ',' + ball_y + ':' + score_a + ',' + score_b);
-                send(sd, reply.c_str(), strlen(reply.c_str()), 0);
+                send(socket_id, reply.c_str(), strlen(reply.c_str()), 0);
                 // send(sd , buffer , strlen(buffer) , 0 );
                 std::cout << std::string(buffer) << std::endl;
                 redis_mtx.lock();
-                redisHandler->set_key(std::to_string(i) + ':' + _player_connected, std::to_string(1));
+                redisHandler->set_key(std::to_string(socket_id) + ':' + _player_connected, std::to_string(1));
                 redis_mtx.unlock();
             }
         }
@@ -90,7 +94,7 @@ void *client_listener(int max_sd, fd_set readfds, int socket_id)
     return NULL;
 }
 
-void wait_for_connections(pthread *client_threads, int master_socket, sockaddr_in address, int max_sd, fd_set readfds)
+void wait_for_connection(pthread_t *client_threads, int &master_socket)
 {
     std::string new_connection = "New User Connected\nSocket FD is %d\nIP : %s:%d\nSocket Index: %d\n";
     int client_count = 0;
@@ -101,13 +105,15 @@ void wait_for_connections(pthread *client_threads, int master_socket, sockaddr_i
         FD_SET(master_socket, &readfds);
         max_sd = master_socket;
 
-        activity = select(max_sd + 1, &readfds, NULL, NULL, timeout);
+        int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
 
         if ((activity < 0) && (errno != EINTR))
         {
             printf("select error");
         }
 
+        int new_socket = 0;
+        int addrlen = sizeof(address);
         if (FD_ISSET(master_socket, &readfds))
         {
             if ((new_socket = accept(master_socket,
@@ -134,6 +140,9 @@ void wait_for_connections(pthread *client_threads, int master_socket, sockaddr_i
 
                 printf(new_connection.c_str(), new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port), i);
 
+                int *socket_id = new int;
+                *socket_id = client_count;
+                pthread_create((client_threads + client_count), NULL, client_listener, static_cast<void *>(socket_id));
                 // pthread_create(*(client_threads + client_count)
                 //  printf("Adding to list of sockets as %d\n" , i);
 
@@ -148,7 +157,7 @@ void wait_for_connections(pthread *client_threads, int master_socket, sockaddr_i
     return;
 }
 
-void initialization(int master_socket)
+void initialization(int &master_socket)
 {
     if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
@@ -196,7 +205,6 @@ int main(int argc, char *argv[])
     int master_socket, addrlen, new_socket,
         max_clients = 2, activity, i, valread, sd,
         client_socket[max_clients];
-    int max_sd;
 
     char buffer[1025]; // data buffer of 1K
 
@@ -211,6 +219,7 @@ int main(int argc, char *argv[])
         client_socket[i] = 0;
     }
 
+    initialization(master_socket);
     // create a master socket
     // accept the incoming connection
     addrlen = sizeof(address);
@@ -222,177 +231,175 @@ int main(int argc, char *argv[])
     //pthread_create(&update_thread, NULL, status_update, NULL);
 
     pthread_t client_threads[max_clients];
-    int client_count = 0;
-    while (true)
-    {
-        FD_ZERO(&readfds);
+    wait_for_connection(client_threads, master_socket);
+    // int client_count = 0;
+    // while (true)
+    // {
+    //     FD_ZERO(&readfds);
 
-        FD_SET(master_socket, &readfds);
-        max_sd = master_socket;
+    //     FD_SET(master_socket, &readfds);
+    //     max_sd = master_socket;
 
-        activity = select(max_sd + 1, &readfds, NULL, NULL, timeout);
+    //     activity = select(max_sd + 1, &readfds, NULL, NULL, timeout);
 
-        if ((activity < 0) && (errno != EINTR))
-        {
-            printf("select error");
-        }
+    //     if ((activity < 0) && (errno != EINTR))
+    //     {
+    //         printf("select error");
+    //     }
 
-        if (FD_ISSET(master_socket, &readfds))
-        {
-            if ((new_socket = accept(master_socket,
-                                     (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-            {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-        }
+    //     if (FD_ISSET(master_socket, &readfds))
+    //     {
+    //         if ((new_socket = accept(master_socket,
+    //                                  (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    //         {
+    //             perror("accept");
+    //             exit(EXIT_FAILURE);
+    //         }
+    //     }
 
-        for (int i = 0; i < max_clients; i++)
-        {
-            // if position is empty
-            if (client_socket[i] == 0)
-            {
-                client_count++;
-                client_socket[i] = new_socket;
-                FD_SET(new_socket, &readfds);
+    //     for (int i = 0; i < max_clients; i++)
+    //     {
+    //         // if position is empty
+    //         if (client_socket[i] == 0)
+    //         {
+    //             client_count++;
+    //             client_socket[i] = new_socket;
+    //             FD_SET(new_socket, &readfds);
 
-                if (new_socket > max_sd)
-                    max_sd = new_socket;
+    //             if (new_socket > max_sd)
+    //                 max_sd = new_socket;
 
-                printf(new_connection.c_str(), new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port), i);
+    //             printf(new_connection.c_str(), new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port), i);
 
-                redisHandler->set_key(std::to_string(i) + ':' + _player_connected, std::to_string(1));
-                // printf("Adding to list of sockets as %d\n" , i);
+    //             redisHandler->set_key(std::to_string(i) + ':' + _player_connected, std::to_string(1));
+    //             // printf("Adding to list of sockets as %d\n" , i);
 
-                break;
-            }
-        }
+    //             break;
+    //         }
+    //     }
 
-        if (client_count == max_clients)
-            break;
-    }
+    //     if (client_count == max_clients)
+    //         break;
+    // }
 
-    while (true)
-    {
-    }
+    // while (true)
+    // {
+    // }
 
-    while (TRUE)
-    {
-        // clear the socket set
-        FD_ZERO(&readfds);
+    // while (TRUE)
+    // {
+    //     // clear the socket set
+    //     FD_ZERO(&readfds);
 
-        // add master socket to set
-        FD_SET(master_socket, &readfds);
-        max_sd = master_socket;
+    //     // add master socket to set
+    //     FD_SET(master_socket, &readfds);
+    //     max_sd = master_socket;
 
-        // add child sockets to set
-        client_socket_mtx.lock();
-        for (i = 0; i < max_clients; i++)
-        {
-            // socket descriptor
-            sd = client_socket[i];
+    //     // add child sockets to set
+    //     client_socket_mtx.lock();
+    //     for (i = 0; i < max_clients; i++)
+    //     {
+    //         // socket descriptor
+    //         sd = client_socket[i];
 
-            // if valid socket descriptor then add to read list
-            if (sd > 0)
-                FD_SET(sd, &readfds);
+    //         // if valid socket descriptor then add to read list
+    //         if (sd > 0)
+    //             FD_SET(sd, &readfds);
 
-            // highest file descriptor number, need it for the select function
-            if (sd > max_sd)
-                max_sd = sd;
-        }
-        client_socket_mtx.unlock();
+    //         // highest file descriptor number, need it for the select function
+    //         if (sd > max_sd)
+    //             max_sd = sd;
+    //     }
+    //     client_socket_mtx.unlock();
 
-        // wait for an activity on one of the sockets , timeout is NULL ,
-        // so wait indefinitely
-        struct timeval *timeout;
-        timeout->tv_sec = 0;
-        timeout->tv_usec = 50;
-        activity = select(max_sd + 1, &readfds, NULL, NULL, timeout);
+    //     // wait for an activity on one of the sockets , timeout is NULL ,
+    //     // so wait indefinitely
+    //     activity = select(max_sd + 1, &readfds, NULL, NULL, timeout);
 
-        if ((activity < 0) && (errno != EINTR))
-        {
-            printf("select error");
-        }
+    //     if ((activity < 0) && (errno != EINTR))
+    //     {
+    //         printf("select error");
+    //     }
 
-        // for (int i = 0; i < max_clients; i++)
-        // {
-        //     send(client_socket[i], reply.c_str(), strlen(reply.c_str()), 0);
-        // }
+    //     // for (int i = 0; i < max_clients; i++)
+    //     // {
+    //     //     send(client_socket[i], reply.c_str(), strlen(reply.c_str()), 0);
+    //     // }
 
-        // If something happened on the master socket ,
-        // then its an incoming connection
-        if (FD_ISSET(master_socket, &readfds))
-        {
-            if ((new_socket = accept(master_socket,
-                                     (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-            {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
+    //     // If something happened on the master socket ,
+    //     // then its an incoming connection
+    //     if (FD_ISSET(master_socket, &readfds))
+    //     {
+    //         if ((new_socket = accept(master_socket,
+    //                                  (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    //         {
+    //             perror("accept");
+    //             exit(EXIT_FAILURE);
+    //         }
 
-            // inform user of socket number - used in send and receive commands
-            // printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
-            //       (address.sin_port));
+    //         // inform user of socket number - used in send and receive commands
+    //         // printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
+    //         //       (address.sin_port));
 
-            // send new connection greeting message
-            // reply = "Connected to marvin.webredirect.org";
-            //  if( send(new_socket, reply.c_str(), strlen(reply.c_str()), 0) != strlen(reply.c_str()) )
-            //  {
-            //      perror("send");
-            //  }
+    //         // send new connection greeting message
+    //         // reply = "Connected to marvin.webredirect.org";
+    //         //  if( send(new_socket, reply.c_str(), strlen(reply.c_str()), 0) != strlen(reply.c_str()) )
+    //         //  {
+    //         //      perror("send");
+    //         //  }
 
-            // puts("Welcome message sent successfully");
+    //         // puts("Welcome message sent successfully");
 
-            // add new socket to array of sockets
-            client_socket_mtx.lock();
+    //         // add new socket to array of sockets
+    //         client_socket_mtx.lock();
 
-            client_socket_mtx.unlock();
-        }
+    //         client_socket_mtx.unlock();
+    //     }
 
-        // else its some IO operation on some other socket
-        for (int i = 0; i < max_clients; i++)
-        {
-            sd = client_socket[i];
+    //     // else its some IO operation on some other socket
+    //     for (int i = 0; i < max_clients; i++)
+    //     {
+    //         sd = client_socket[i];
 
-            if (FD_ISSET(sd, &readfds))
-            {
-                // Check if it was for closing , and also read the
-                // incoming message
-                valread = read(sd, buffer, 1025);
-                if (std::string(buffer).substr(0, 3) == "~~~")
-                {
-                    // Somebody disconnected , get his details and print
-                    getpeername(sd, (struct sockaddr *)&address,
-                                (socklen_t *)&addrlen);
-                    printf("Host disconnected , ip %s , port %d \n",
-                           inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+    //         if (FD_ISSET(sd, &readfds))
+    //         {
+    //             // Check if it was for closing , and also read the
+    //             // incoming message
+    //             valread = read(sd, buffer, 1025);
+    //             if (std::string(buffer).substr(0, 3) == "~~~")
+    //             {
+    //                 // Somebody disconnected , get his details and print
+    //                 getpeername(sd, (struct sockaddr *)&address,
+    //                             (socklen_t *)&addrlen);
+    //                 printf("Host disconnected , ip %s , port %d \n",
+    //                        inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
-                    // Close the socket and mark as 0 in list for reuse
-                    close(sd);
-                    client_socket_mtx.lock();
-                    client_socket[i] = 0;
-                    client_socket_mtx.unlock();
-                }
+    //                 // Close the socket and mark as 0 in list for reuse
+    //                 close(sd);
+    //                 client_socket_mtx.lock();
+    //                 client_socket[i] = 0;
+    //                 client_socket_mtx.unlock();
+    //             }
 
-                // Echo back the message that came in
-                else
-                {
-                    // set the string terminating NULL byte on the end
-                    // of the data read
-                    buffer[valread] = '\0';
+    //             // Echo back the message that came in
+    //             else
+    //             {
+    //                 // set the string terminating NULL byte on the end
+    //                 // of the data read
+    //                 buffer[valread] = '\0';
 
-                    y_pos = atoi(buffer);
-                    reply_mtx.lock();
-                    reply = '<' + std::to_string(y_pos) + ':' + std::to_string(ball_x) + ',' + std::to_string(ball_y) + ':' + std::to_string(score_a) + ',' + std::to_string(score_b) + '>';
-                    // std::string(y_pos + ':' + ball_x + ',' + ball_y + ':' + score_a + ',' + score_b);
-                    reply_mtx.unlock();
-                    send(sd, reply.c_str(), strlen(reply.c_str()), 0);
-                    // send(sd , buffer , strlen(buffer) , 0 );
-                    std::cout << std::string(buffer) << std::endl;
-                }
-            }
-        }
-    }
+    //                 y_pos = atoi(buffer);
+    //                 reply_mtx.lock();
+    //                 reply = '<' + std::to_string(y_pos) + ':' + std::to_string(ball_x) + ',' + std::to_string(ball_y) + ':' + std::to_string(score_a) + ',' + std::to_string(score_b) + '>';
+    //                 // std::string(y_pos + ':' + ball_x + ',' + ball_y + ':' + score_a + ',' + score_b);
+    //                 reply_mtx.unlock();
+    //                 send(sd, reply.c_str(), strlen(reply.c_str()), 0);
+    //                 // send(sd , buffer , strlen(buffer) , 0 );
+    //                 std::cout << std::string(buffer) << std::endl;
+    //             }
+    //         }
+    //     }
+    // }
 
     return 0;
 }
