3badf0c16a636061a697b2f5579e250e08366d01
EngimaticThrakon <gbrown92799@gmail.com>
2022-04-04 11:23:49 -0400

    Removed shared file block from gitignore

---
+++ server-dev/redis_shared_files/redis_keys.h
@@ -0,0 +1,17 @@
+#ifndef REDIS_KEYS_H
+#define REDIS_KEYS_H
+
+#include <string>
+
+std::string _window_size = "window:size";
+std::string _paddle_size = "paddle:size";
+std::string _ball_size = "ball:size";
+
+std::string _ball_location = "ball:location";
+std::string _paddle_location = "paddle_location";
+
+std::string _player_connected = "player:connected";
+
+std::string _game_started = "game:started";
+
+#endif
