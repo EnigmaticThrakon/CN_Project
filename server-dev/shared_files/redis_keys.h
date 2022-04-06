4a9aa13dce0a4124f1f3282b11893dc6ae1f77b7
EngimaticThrakon <gbrown92799@gmail.com>
2022-04-06 12:38:08 -0400

    Added redis keys

--- server-dev/shared_files/redis_keys.h
+++ server-dev/shared_files/redis_keys.h
@@ -14,8 +14,12 @@ std::string _player_connected = "player:connected";
 std::string _player_transmitting = "player:transmitting";
 std::string _player_response = "player:response";
 
-std::string _game_started = "game:started";
-
 std::string _test_response = "test:response";
+
+std::string _left_player_connected = "left:player:connected";
+std::string _right_player_connected = "right:player:connected";
+
+std::string _game_started = "game:started";
+std::string _game_setup = "game:setup";
 
 #endif
