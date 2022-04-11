0a52bc6d2a0fa99d4e6cc58d154141bf9acdd925
EngimaticThrakon <gbrown92799@gmail.com>
2022-04-10 20:53:37 -0400

    Updated logic for how the game will operate and how it gets setup for two players

--- server-dev/pong_controller/pong_controller.cpp
+++ server-dev/pong_controller/pong_controller.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <hiredis/hiredis.h>
-#include "shared_files/redis_functions.h"
-#include "shared_files/redis_keys.h"
-#include "shared_files/timer.h"
-#include "shared_files/game.h"
-#include "shared_files/game_constants.h"
+#include "../shared_files/redis_functions.h"
+#include "../shared_files/redis_keys.h"
+#include "../shared_files/timer.h"
+#include "../shared_files/game.h"
+#include "../shared_files/game_constants.h"
 
 redis_handler *redisHandler = new redis_handler();
 pong_game *gameHandler = nullptr;
@@ -42,6 +42,8 @@ void await_players()
         playersConnected = playersConnected && test_player_connection(false);
     }
 
+    snprintf(_status_buffer, sizeof(_status_buffer), responseFormat.c_str(), "999", "999", "999", "999", "999", "999");
+
     return;
 }
 
@@ -55,8 +57,10 @@ std::string get_response_string(std::string formattedString)
 
 void update_game_status()
 {
-    snprintf(_status_buffer, sizeof(_status_buffer), responseFormat.c_str(), gameHandler->get_lpaddle_location(), gameHandler->get_rpaddle_location(),
-            gameHandler->get_ball_location_x(), gameHandler->get_ball_location_y(), gameHandler->get_left_score(), gameHandler->get_right_score());
+    snprintf(_status_buffer, sizeof(_status_buffer), responseFormat.c_str(), gameHandler->get_lpaddle_location().c_str(), gameHandler->get_rpaddle_location().c_str(),
+            gameHandler->get_ball_location_x().c_str(), gameHandler->get_ball_location_y().c_str(), gameHandler->get_left_score().c_str(), gameHandler->get_right_score().c_str());
+
+    std::cout << _status_buffer << std::endl;
 
     redisHandler->set_key(_game_status, get_response_string(_status_buffer));
 }
@@ -69,9 +73,12 @@ int main()
         setup_game();
         await_players();
 
-        if(redisHandler->get_key(_game_started) == "1")
+        if(redisHandler->get_key(_game_setup) == "1")
         {
             game_running = true;
+            redisHandler->set_key(_left_player_response, std::to_string(_initial_lpaddle_y));
+            redisHandler->set_key(_right_player_response, std::to_string(_initial_rpaddle_y));
+            redisHandler->set_key(_game_started, "1");
         }
 
         globalTimer->reset();
@@ -80,10 +87,14 @@ int main()
             gameHandler->update_paddle_location(stoi(redisHandler->get_key(_left_player_response)), true);
             gameHandler->update_paddle_location(stoi(redisHandler->get_key(_right_player_response)), false);
 
-            if(globalTimer->elapsed_time() > 10)
+            if(globalTimer->elapsed_time() > 100)
             {
-                gameHandler->update_ball_location();
                 globalTimer->reset();
+                if(gameHandler->update_ball_location())
+                {
+                    update_game_status();
+                    while(globalTimer->elapsed_time() < 1000) { }
+                }
             }
 
             update_game_status();
@@ -94,15 +105,15 @@ int main()
                 redisHandler->set_key(_game_started, "0");
             }
 
-            if(!test_player_connection(true) || !test_player_connection(false))
-            {
-                game_running = false;
-                redisHandler->set_key(_game_started, "0");
-                break;
-            }
+            //if(!test_player_connection(true) || !test_player_connection(false))
+            //{
+            //    game_running = false;
+            //    redisHandler->set_key(_game_started, "0");
+            //    break;
+            //}
         }
+        break;
 
     }
-    std::cout << "Hello World!";
     return 0;
 }
