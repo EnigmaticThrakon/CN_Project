e515aff61fcc8f1344546053c967c6384a8984d4
EngimaticThrakon <gbrown92799@gmail.com>
2022-04-16 14:55:25 -0400

    Modified the screen height to prevent the ball from traveling outside the viewabe area. Added comments for referencing in the report. Added the string formatting for when a player connects or disconnects

--- server-dev/shared_files/game_constants.h
+++ server-dev/shared_files/game_constants.h
@@ -1,19 +1,24 @@
 #ifndef GAME_CONSTANTS_H
 #define GAME_CONSTANTS_H
 
-int _screen_height = 800;
+//Header file that stores basic constants of the game
+//      to allow for easier access and manipulation of global
+//      values that are the basis for the game and communication
+
+//Pair of variables that define the window dimensions
+int _screen_height = 780;
 int _screen_width = 1000;
 
-int _rscore_x = 520;
-int _lscore_x = 470;
-
+//Variables that determine the dimensions of the paddle components
+//      and their initial locations
 int _paddle_height = 100;
 int _paddle_width = 20;
+int _initial_paddle_y = _screen_height / 2;
 int _initial_lpaddle_x = 20;
-int _initial_lpaddle_y = _screen_height / 2;
 int _initial_rpaddle_x = 970;
-int _initial_rpaddle_y = _screen_height / 2;
 
+//Variables that determine the dimensions of the ball component
+//      and the ball's initial location along with velocities
 int _ball_height = 10;
 int _ball_width = 10;
 int _initial_ball_velocity_x = 1;
@@ -21,8 +26,15 @@ int _initial_ball_velocity_y = 1;
 int _initial_ball_x = _screen_width / 2;
 int _initial_ball_y = _screen_height / 2;
 
+//Standardized response format for all the information that is 
+//      sent to the clients from the server
 std::string responseFormat = "<%s,%s:%s,%s:%s,%s>";
 
+//Standardized console output for when users connect or disconnect
+std::string newConnectionFormat = "New User Connected\n%s Player\nSocket FD is %d\nIP : %s:%d\n";
+std::string disconnectionFormat = "User Disconnected\n%s Player\nSocket FD is %d\nIP : %s:%d\n";
+
+//Variable that determines the score before the game completes
 int _win = 5;
 
 #endif
